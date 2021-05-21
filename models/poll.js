add1bf2f28b1f9e198088991e0c51dc6d295df9d
Emily Scudero <emily@MSI.COM>
2021-05-21 13:50:11 -0400

    merge

---
+++ models/poll.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const Poll = sequelize.define("poll", {
+    question: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    response1: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    response2: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    response3: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    response4: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    userID: {
+        type: DataTypes.INTEGER
+    }
+  });
+  return Poll;
+};
