e6359840742a7e7c2e41ab84299b11c6050c0400
Emily Scudero <emily@MSI.COM>
2021-05-22 15:37:56 -0400

    user and admin defined roles

--- controllers/pollcontroller.js
+++ controllers/pollcontroller.js
@@ -1,11 +1,11 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-const Poll = require('../db').import('../models/poll');
+const Poll = require("../db").import("../models/poll");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 var sequelize = require("../db");
-let validateSession = (require('../middleware/validate-sessions'));
+let validateSession = require("../middleware/validate-sessions");
 
 /* Endpoints
 
@@ -19,75 +19,88 @@ http://localhost:3000/poll/getAll - GET
 /*****************
  * POLL - CREATE
  ****************/
- router.post('/create', validateSession, (req,res) => {
+router.post("/create", validateSession, (req, res) => {
+  if (req.user.role === "admin") {
     const pollEntry = {
-        question: req.body.question,
-        response1: req.body.response1,
-        response2: req.body.response2,
-        response3: req.body.response3,
-        response4: req.body.response4,
-        userID: req.user.id
-    }
+      question: req.body.question,
+      response1: req.body.response1,
+      response2: req.body.response2,
+      response3: req.body.response3,
+      response4: req.body.response4,
+      userID: req.user.id,
+    };
     Poll.create(pollEntry)
-    .then((poll) => {
+      .then((poll) => {
         res.status(200).json({
-            poll: poll,
-            message: "New poll created."
-        })
-    })
-    .catch(err => res.status(500).json({error: err}))
-
-})
-
-
-/********************
-* POLL - DELETE  *
-*******************/
-router.delete('/delete/:id', validateSession, (req,res) => {
-    const query = { where: { id: req.params.id, userID: req.user.id} };
-
-    Poll.destroy(query)
-    .then((response) => res.status(200).json({ 
-        message: response > 0 ?  " Poll deleted": "Deleting poll failed."
-    }))
-    .catch((err) => res.status(500).json({error: err}));
+          poll: poll,
+          message: "New poll created.",
+        });
+      })
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.json({ message: "Not an Admin" });
+  }
 });
 
+/********************
+ * POLL - DELETE  *
+ *******************/
+router.delete("/delete/:id", validateSession, (req, res) => {
+  if (req.user.role === "admin") {
+    const query = { where: { id: req.params.id, userID: req.user.id } };
+
+    Poll.destroy(query)
+      .then((response) =>
+        res.status(200).json({
+          message: response > 0 ? " Poll deleted" : "Deleting poll failed.",
+        })
+      )
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.json({ message: "Not an Admin" });
+  }
+});
 
 /******************
  * POLL - UPDATE  *
  *****************/
- router.put('/update/:id', validateSession, (req,res) => {
+router.put("/update/:id", validateSession, (req, res) => {
+  if (req.user.role === "admin") {
     const updatePollEntry = {
-        question: req.body.question,
-        response1: req.body.response1,
-        response2: req.body.response2,
-        response3: req.body.response3,
-        response4: req.body.response4
-  };
-  
-  const query = { where: { id: req.params.id, userID: req.user.id} };
-  
-  Poll.update(updatePollEntry, query)
-  .then((poll) => res.status(200).json({
-      message: poll > 0? "Poll updated" : "Couldn't update this poll"})
-  )
-  .catch((err) => res.status(500).json({error: err}));
-  })
+      question: req.body.question,
+      response1: req.body.response1,
+      response2: req.body.response2,
+      response3: req.body.response3,
+      response4: req.body.response4,
+    };
 
+    const query = { where: { id: req.params.id, userID: req.user.id } };
 
+    Poll.update(updatePollEntry, query)
+      .then((poll) =>
+        res.status(200).json({
+          message: poll > 0 ? "Poll updated" : "Couldn't update this poll",
+        })
+      )
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.json({ message: "Not an Admin" });
+  }
+});
 
 /****************
-* POLL - GETALL  *
-*****************/
-router.get('/getAll', validateSession, (req,res) => {
+ * POLL - GETALL  *
+ *****************/
+router.get("/getAll", validateSession, (req, res) => {
+  if (req.user.role === "admin") {
     Poll.findAll({
-        where: { userID: req.user.id }
+      where: { userID: req.user.id },
     })
-    .then(polls => res.status(200).json(polls))
-    .catch(err => res.status(500).json({error: err}))
-})
+      .then((polls) => res.status(200).json(polls))
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.json({ message: "Not an Admin" });
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
--- controllers/pollrespcontroller.js
+++ controllers/pollrespcontroller.js
@@ -1,14 +1,90 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-const User = require('../db').import('../models/user');
+const User = require("../db").import("../models/user");
+const Responses = require("../db").import("../models/pollresponses");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
+var sequelize = require("../db");
+let validateSession = require("../middleware/validate-sessions");
 
-router.get('/test', function(req, res){
-    res.send("this is a test from pollrespcontroller")
-})
+/* Endpoints
+http://localhost:3000/responses/select/:poll_id - POST
+http://localhost:3000/responses/getAllResp/:poll_id - GET
+http://localhost:3000/responses/countAll/:poll_id - GET
+*/
 
+/********************************
+ * RESPONSES - SELECT
+ * -> vote for a specific poll
+ *
+ * Used for:
+ * - As a user, I want to select a single item from the list of choices, so I can cast my vote
+ *******************************/
+router.post("/select/:poll_id", validateSession, (req, res) => {
+  if (req.user.role === "user" || req.user.role === "admin") {
+    const selection = {
+      pollID: req.params.poll_id,
+      pollSelection: req.body.selection,
+      userID: req.user.id,
+    };
 
+    Responses.create(selection)
+      .then((selection) => {
+        res.status(200).json({
+          pollSelection: selection,
+          message: "User voted.",
+        });
+      })
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.json({ message: "Not a User" });
+  }
+});
 
-module.exports = router
+/*******************************************************************************************
+ * RESPONSES - GETALL
+ * -> get all votes for a specific poll
+ *
+ * Used for:
+ * - As an admin, I want to be able to see the results of the polls, so I can know what our team members think
+ *******************************************************************************************/
+router.get("/getAllResp/:poll_id", validateSession, (req, res) => {
+  if (req.user.role === "admin") {
+    const query = {
+      where: { pollID: req.params.poll_id },
+    };
+
+    Responses.findAll(query)
+      .then((polls) => res.status(200).json(polls))
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.json({ message: "Not an Admin" });
+  }
+});
+
+/*********************************************************************************************
+ * RESPONSES - COUNTALL
+ * -> count all votes for a specific poll
+ *
+ * Used for:
+ * - As a user, I want to see the vote totals after I submit mine, so I can see the current results
+ *********************************************************************************************/
+router.get("/countAll/:poll_id", validateSession, (req, res) => {
+  if (req.user.role === "user" || req.user.role === "admin") {
+    Responses.count({
+      where: { pollID: req.params.poll_id },
+    })
+      .then((nbVotes) =>
+        res.status(200).json({
+          votes: nbVotes,
+          message: "Votes counted!.",
+        })
+      )
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.json({ message: "Not a User" });
+  }
+});
+
+module.exports = router;
