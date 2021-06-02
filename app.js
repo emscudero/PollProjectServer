c2e429f527cff83b2d450e1f8c8e027cf8dd3a27
Erin Jeter <ekjeter@gmail.com>
2021-06-02 12:35:04 -0400

    fixes update in poll controller

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 require("dotenv").config();
-let express = require('express');
+let express = require("express");
 let app = express();
-let sequelize = require('./db');
+let sequelize = require("./db");
 
-let user = require('./controllers/usercontroller');
-let poll = require('./controllers/pollcontroller');
-let responses = require('./controllers/pollrespcontroller');
+let user = require("./controllers/usercontroller");
+let poll = require("./controllers/pollcontroller");
+let responses = require("./controllers/pollrespcontroller");
 
 sequelize.sync();
-// sequelize.sync({force:true}) //to force reset db in pgAdmin
+// sequelize.sync({ force: true }); //to force reset db in pgAdmin
 
-app.use(require('./middleware/headers'));
+app.use(require("./middleware/headers"));
 app.use(express.json());
 
 // /****
 //  *  EXPOSED route *
 //  ****/
- app.use('/user', user);
+app.use("/user", user);
 
- /****
-  *  PROTECTED route * - need a token
-  ****/
- app.use('/poll', poll);
- app.use('/responses', responses);
- 
+/****
+ *  PROTECTED route * - need a token
+ ****/
+app.use("/poll", poll);
+app.use("/responses", responses);
 
- app.listen(3000, function(){
-     console.log('App is listening on port 3000');
- }) 
+app.listen(3000, function () {
+  console.log("App is listening on port 3000");
+});
